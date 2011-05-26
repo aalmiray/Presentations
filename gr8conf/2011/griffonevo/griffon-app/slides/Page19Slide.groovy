cea369036e6b6764785ee99df258bee8a50eb1af
aalmiray <aalmiray@gmail.com>
2011-05-26 12:32:50 +0200

    Remove unneeded code from slide 19

--- gr8conf/2011/griffonevo/griffon-app/slides/Page19Slide.groovy
+++ gr8conf/2011/griffonevo/griffon-app/slides/Page19Slide.groovy
@@ -1,28 +1,6 @@
 import griffon.transitions.FadeTransition2D
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver
 
-def (m, v, c) = createMVCGroup('GroovyCodeEditor', 'editor-slide18')
-m.editable = false
-m.code = '''panel {
-    stackLayout()
-    gradientPanel(constraints: BOTTOM)
-    imageFlow(id: "flow", constraints: TOP) {
-        icons.each { icon ->
-            imageFlowItem(resource: "/images/everaldo/${icon}.png",
-                          label: icon[0].toUpperCase() + icon[1..-1])
-        }
-    }
-    noparent{ flow.selectedIndex = 0 }
-}'''
-
-ClassLoader classLoader = getClass().classLoader
-String path = 'classpath*:/images/everaldo/*.png'
-List icons = new PathMatchingResourcePatternResolver(classLoader)
-                      .getResources(path).collect([]) { r ->
-    def (matcher, icon) = (r.getURL().path =~ /.*\/([a-zA-Z0-9_\-]+)\.png/)[0]
-    icon
-}
-
 slide(title: '', backgroundPainter: backgroundPainter, transition: new FadeTransition2D()) {
     migLayout(layoutConstraints: 'fill',
               columnConstraints: '5%[center]5%',
