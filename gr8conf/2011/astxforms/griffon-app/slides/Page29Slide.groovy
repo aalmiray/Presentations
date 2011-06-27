1a9d1c9832aa73be9e2a73d02aed48e8dfaee717
aalmiray <aalmiray@gmail.com>
2011-06-27 09:15:37 -0500

    Added a slide with app stats

--- gr8conf/2011/astxforms/griffon-app/slides/Page29Slide.groovy
+++ gr8conf/2011/astxforms/griffon-app/slides/Page29Slide.groovy
@@ -2,7 +2,18 @@ slide(title: '', backgroundPainter: backgroundPainter) {
     migLayout(layoutConstraints: 'fill',
               columnConstraints: '5%[center]5%',
               rowConstraints: '[bottom]5%[center]5%[top]')
-    label(cssClass: 'header', constraints: 'wrap', 'Thank you!')
-    label(icon: imageIcon('/griffon-in-action.jpg'), constraints: 'wrap')
-    label(cssClass: 'header', '@aalmiray', foreground: Color.BLUE)
+    label(icon: imageIcon('/griffon.png'), cssClass: 'H1', constraints: 'wrap')
+    label(cssClass: 'header', constraints: 'wrap', 'http://bit.ly/mDgXQE', foreground: Color.BLUE)
+    panel(opaque: false) {
+        vbox {
+            label('+----------------------+-------+-------+', cssClass: 'code')
+            label('| Name                 | Files |  LOC  |', cssClass: 'code')
+            label('+----------------------+-------+-------+', cssClass: 'code')
+            label('| Lifecycle            |     5 |     3 |', cssClass: 'code') 
+            label('| Slides               |    31 |   584 |', cssClass: 'code') 
+            label('+----------------------+-------+-------+', cssClass: 'code')
+            label('| Totals               |    36 |   587 |', cssClass: 'code') 
+            label('+----------------------+-------+-------+', cssClass: 'code')
+        }
+    }
 }
